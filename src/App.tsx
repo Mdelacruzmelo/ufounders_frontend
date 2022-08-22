3aa26791caa62f032e809b4eb173413803723b99
Mdelacruzmelo <mdelacruzmelo@gmail.com>
2022-08-22 23:17:17 +0200

    TASK-1 Project strcuture initialized

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, useRoutes } from 'react-router-dom'
+import Clients from './pages/Clients'
 
-function App() {
+const App = () => useRoutes([
+  { path: '/', element: <Clients /> }
+])
+
+const Wrapper = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <App />
+    </Router>
+  )
 }
 
-export default App;
+export default Wrapper;
