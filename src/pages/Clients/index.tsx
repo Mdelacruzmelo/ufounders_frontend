bdad2b19cf764ebb05f42443e82694d668de98e0
Mdelacruzmelo <mdelacruzmelo@gmail.com>
2022-08-24 21:50:09 +0200

    TASK-3 Loading content by api implemented

--- src/pages/Clients/index.tsx
+++ src/pages/Clients/index.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import FadeIn from 'react-fade-in'
 import { useAppSelector } from 'src/hooks/redux'
+import BarLoader from 'react-spinners/BarLoader'
+import { FaRegFolderOpen } from "react-icons/fa";
 import { Header, Sidebar, Client, Details, Modal } from 'src/components'
 import './Clients.scss'
 import { useGetClientsQuery } from './api'
@@ -12,6 +14,36 @@ const Clients = () => {
     const [sidebarOpened, setSidebarOpened] = useState(false)
     const [activePage, setActivePage] = useState(1)
     const { list } = useAppSelector((state) => state.clients)
+
+    const Result = () => {
+
+        if (isLoading) {
+            return (
+                <div className="centered">
+                    <BarLoader color="#000000" loading={true} width={200} />
+                </div>
+            )
+        }
+        if (!isLoading && !data) {
+            return (
+                <div className="centered">
+                    <h1>
+                        <FaRegFolderOpen />
+                    </h1>
+                    <div>
+                        Data not found
+                    </div>
+                </div>
+            )
+        }
+
+        return (
+            <>
+                {list.map((client, index) => <Client client={client} key={index} />)}
+            </>
+        )
+
+    }
 
     return (
         <>
@@ -26,7 +58,7 @@ const Clients = () => {
                     <main className={`${sidebarOpened ? 'sidebar_opened' : ''} `}>
                         <Header />
                         <div className='clients_list'>
-                            {list.map((client, index) => <Client client={client} key={index} />)}
+                            <Result />
                         </div>
                     </main>
                 </div>
